a871a12f6141f66fc11a2c0ea8109dc3af47fa2b
erik286 <108467481+erik286@users.noreply.github.com>
2022-07-26 22:03:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('nonsense')
+streamlit.title('nonsense nonsense')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
