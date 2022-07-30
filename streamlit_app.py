f2e4221ea7d9023db01dd158ad521aeae5633fab
erik286 <108467481+erik286@users.noreply.github.com>
2022-07-30 11:10:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,16 +20,23 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice=streamlit.text_input('What Fruit?','Kiwi')
-streamlit.write('the user answered',fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# streamlit.text(fruityvice_response.json())
+Try:
+  fruit_choice=streamlit.text_input('What Fruit?','Kiwi')
+if not fruitchoice: 
+  streamlit.write('the user answered',fruit_choice)
+else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+  # streamlit.text(fruityvice_response.json())
 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # write your own comment - what does this do?
+  streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
+  
+ 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
